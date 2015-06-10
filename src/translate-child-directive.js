64cb6428d3afdce389be00b652462a85f8007475
Steven Nance <steven@devtrw.com>
2015-06-09 20:15:16 -0700

    rewrite `translate-child` and replace attirbutes with translate format
    `translate-child-into` and `translate-child-key-into` have been removed.
    To translate into an attribute you now specify it before the translation
    key in the `translate-child` attribute as so:
    ````
    <a translate-child="(aria-label,title,!translate)=SOME_TRANSLATION.KEY"></a>
    ````
    
    The attributes should be comma delimited and prefixed with a `!` when you
    want the translation key to be set instead of the translated value.

--- src/translate-child-directive.js
+++ src/translate-child-directive.js
@@ -1,5 +1,56 @@
 function dtrwTranslateChildDirective($compile, $translate) {
 
+  function setValueForAttrs(element, targetAttrs, value) {
+    targetAttrs.forEach((targetAttrs) => {
+      element.attr(targetAttrs, value);
+    });
+  }
+
+  function parseTranslateChildAttributes(configString) {
+    let keyAttributes = [];
+    let valueAttributes = [];
+    let attributes = configString
+      .replace(/[\[\]\(\)\s]+/g, '') // remove parenthesis, brackets, an spaces
+      .split(',');
+
+    attributes.forEach((attributeName) => {
+      // attribute names prefixed with ! signifies that the translation key should be
+      // set instead of the translated value
+      if ('!' === attributeName.charAt(0)) {
+        keyAttributes.push(attributeName.substr(1));
+      } else {
+        valueAttributes.push(attributeName);
+      }
+    });
+
+    return [keyAttributes, valueAttributes];
+  }
+
+  function parseTranslateChildAttr(attribute) {
+    console.log('parsing: ', attribute);
+    let parts = attribute.split('=');
+
+    // by default we just set the translation key on the `translate` attributes
+    if (1 === parts.length) {
+      return {
+        translateKey:    parts[0],
+        keyAttributes:   ['translate'],
+        valueAttributes: []
+      };
+    }
+
+    let parsedAttributes = parseTranslateChildAttributes(parts[0]);
+
+    let parsed = {
+      translateKey:    parts[1],
+      keyAttributes:   parsedAttributes[0],
+      valueAttributes: parsedAttributes[1]
+    };
+    console.log('parsed: ', parsed);
+
+    return parsed;
+  }
+
   return {
     restrict: 'A',
     replace:  false,
@@ -9,30 +60,27 @@ function dtrwTranslateChildDirective($compile, $translate) {
     // Make sure this directive is run first
     priority: 1000,
     link:     function link(scope, element, attrs, translateBaseCtrl) { // jshint ignore:line
-      let {translateChildInto, translateChildKeyInto, translateValues} = attrs;
-      let translateKey = translateBaseCtrl ?
-        translateBaseCtrl.getTranslationKey(attrs.translateChild) :
-        `COULD_NOT_FIND_TRANSLATE_BASE_CONTROLLER_FOR_CHILD-${attrs.translateChild}`;
+      let {
+        translateKey,
+        keyAttributes,
+        valueAttributes
+        } = parseTranslateChildAttr(attrs.translateChild);
 
-      function setValueForAttrs(targetAttrs, value) {
-        targetAttrs
-          .split(',')
-          .forEach((targetAttrs) => {
-            element.attr(targetAttrs, value);
-          });
+      translateKey = translateBaseCtrl ?
+        translateBaseCtrl.getTranslationKey(translateKey) :
+        `COULD_NOT_FIND_TRANSLATE_BASE_CONTROLLER_FOR_CHILD-${translateKey}`;
+
+      if (0 < keyAttributes.length) {
+        setValueForAttrs(element, keyAttributes, translateKey);
       }
 
-      if (translateChildInto) {
-        translateValues = (translateValues) ? scope.$eval(translateValues) : {};
-        setValueForAttrs(translateChildInto, $translate.instant(translateKey, translateValues));
-      }
-
-      if (translateChildKeyInto) {
-        setValueForAttrs(translateChildKeyInto, translateKey);
-      }
-
-      if (!translateChildInto && !translateChildKeyInto) {
-        element.attr('translate', translateKey);
+      if (0 < valueAttributes.length) {
+        let translateValues = (attrs.translateValues) ? scope.$eval(attrs.translateValues) : {};
+        setValueForAttrs(
+          element,
+          valueAttributes,
+          $translate.instant(translateKey, translateValues)
+        );
       }
 
       element.removeAttr('translate-child'); //remove the attribute to avoid indefinite loop
